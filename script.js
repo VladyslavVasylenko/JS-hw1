f26c0c2580bdd6f5d983febe3ccc058a9840e310
vladyslavvasylenko <vladyslavvasylenko03@gmail.com>
2021-04-02 10:13:27 +0300

    new file

---
+++ script.js
@@ -0,0 +1,13 @@
+let age = prompt('Сколько вам лет?', 0);
+if (age >= 0 && age < 12){
+  console.log('Ты ребёнок');
+}
+else if (age >= 12 && age < 18){
+  console.log('Ты подросток');
+}
+else if (age >= 18 && age < 60){
+  console.log('Ты взрослый');
+}
+else if (age >= 60){
+  console.log('Вы пенсионер');
+}
