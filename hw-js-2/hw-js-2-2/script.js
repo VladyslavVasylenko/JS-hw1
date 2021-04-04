3efd5a9d2f95ce06737517d141fb2db12edcaca8
Vasylenko Vladyslav <vasylenkovladyslav03@gmail.com>
2021-04-04 18:44:11 +0300

    new file

--- hw-js-2/hw-js-2-2/script.js
+++ hw-js-2/hw-js-2-2/script.js
@@ -1,14 +1,14 @@
-function gcd(a,b) {
-  a = Math.abs(a);
-  b = Math.abs(b);
-  if (b > a) {let temp = a; a = b; b = temp;}
-  while (true) {
-      if (b == 0) return a;
-      a %= b;
-      if (a == 0) return b;
-      b %= a;
-  }
-}
+"use strict";
 
 let userNumberFirst = prompt('укажите первое ваше число', '');
 let userNumberSecond = prompt('укажите второе ваше число', '');
+
+let gcd = function(a, b) {
+  if ( ! b) {
+      return a;
+  }
+
+  return gcd(b, a % b);
+};
+
+console.log(gcd( userNumberFirst, userNumberSecond));
---
+++ hw-js-2/hw-js-2-3/script.js
@@ -0,0 +1,18 @@
+"use stict"
+
+let n = prompt('Your number?'); 
+let divisor = [];
+
+if (n % 1 == 0) {
+  divisor.push(1);
+  n=Math.round(n/1);
+  let max=n/2;
+  for (let i = 2; i <= max; i++) {
+    if (n % i == 0) {
+      divisor.push(1*i);
+    };
+  };
+  divisor.push(1*n);
+};
+
+console.log(divisor);
