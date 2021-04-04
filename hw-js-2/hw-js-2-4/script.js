a9bb75605328d24985423923102e4adf17b2e584
Vasylenko Vladyslav <vasylenkovladyslav03@gmail.com>
2021-04-04 20:14:24 +0300

    fixes

--- hw-js-2/hw-js-2-4/script.js
+++ hw-js-2/hw-js-2-4/script.js
@@ -1,4 +1,11 @@
 "use strict";
 
-let userNumber = prompt('Your number?', ' ');
+let userNumber = prompt('Your number?', '');
 
+/*console.log(String(userNumber).length);*/
+
+function numDigits(x) {
+  return Math.max(Math.floor(Math.log10(Math.abs(x))), 0) + 1;
+}
+
+console.log(numDigits(userNumber));
