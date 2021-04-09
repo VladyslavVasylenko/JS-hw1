996f8f2aa75828fc1dd11a6b31ba19533ba4bb27
VladyslavVasylenko <vladyslavvasylenko03@gmail.com>
2021-04-09 19:27:11 +0300

    first task

--- hw-js-4 copy/hw-js-4-1/script.js
+++ hw-js-4 copy/hw-js-4-1/script.js
@@ -1,4 +1,4 @@
-"use strict";
+
 
 let car = {
   manufacturer: "Audi",
@@ -7,14 +7,38 @@ let car = {
   "average speed": 80,
 }
 
-function ourCar() {
-  let a = "Manufacturer: ";
-  let b = " car model ";
-  let c = " year of issue ";
-  let d = " average speed ";
+function ourCar(obj) {
   
-  document.write("Manufacturer: " + car[manufacturer] + " car model " + car[model] + " year of issue " + car["year of issue"] + " average speed " + car["average speed"]);
-  return a,b,c,d;
+  let result = "";
+  for (let i in obj) {
+    if (obj.hasOwnProperty(i)) {
+      result += i + " - " + obj[i] + "<br>";
+    }
+  }
+
+  document.write(result);
+  return result;
 }
 
-ourCar();
+ourCar(car);
+
+let user = +prompt('Which route to calculate: ');
+
+function road(distance){
+  let a = distance;
+  let speed = car["average speed"];
+  let relax = 1;
+  if (a / speed < 4) {
+    document.write("No relax.");
+  }
+  else if(a / speed > 4 ){
+    let result = a / speed + relax;
+    document.write("You need " + result + " hour.");
+  } 
+  else if(a / speed > 10){
+    result = a / speed + relax + 1;
+    document.write("You need " + result + " hour.");
+  }
+}
+
+road(user);
