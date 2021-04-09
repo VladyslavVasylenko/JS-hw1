e6b63b2f07f59d2bec8f17b4e79e017d713bfaf2
Vasylenko Vladyslav <vasylenkovladyslav03@gmail.com>
2021-04-09 10:42:15 +0300

    fixes

--- hw-js-2/hw-js-2-10/script.js
+++ hw-js-2/hw-js-2-10/script.js
@@ -1,11 +1,11 @@
 "use strict";
-/*function send() {
+function send() {
   let n, pcNum, out, tempOut;
 
   pcNum = Math.floor(( Math.random() * 100 ) + 1);
   tempOut = document.getElementById( 'temp-out' );
   tempOut.innerHTML = pcNum;
-
+  
 
   n = document.getElementById( 'userNum' ).value;
   out = document.getElementById( 'out' );
@@ -19,5 +19,5 @@
   if (pcNum == n) {
     out.innerHTML = 'pc guess your number' + number;
   }
-}*/
+}
 //Проблема с этим заданием не понял до конца как можно выполнить условие, по этому решил выполнить хотя бы так.
