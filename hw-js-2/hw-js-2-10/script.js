4447f9b06c977129ccf6d68255d424bb6294ea29
Vasylenko Vladyslav <vasylenkovladyslav03@gmail.com>
2021-04-09 10:39:41 +0300

    fixes

--- hw-js-2/hw-js-2-10/script.js
+++ hw-js-2/hw-js-2-10/script.js
@@ -1,5 +1,5 @@
 "use strict";
-function send() {
+/*function send() {
   let n, pcNum, out, tempOut;
 
   pcNum = Math.floor(( Math.random() * 100 ) + 1);
@@ -17,7 +17,7 @@ function send() {
     out.innerHTML = 'pc indicated a number greater than yours';
   } 
   if (pcNum == n) {
-    out.innerHTML = 'pc guess your number';
+    out.innerHTML = 'pc guess your number' + number;
   }
-}
-//Проблема с этим заданием не понял до конца как можно выполнить условие до конца, по этому решил выполнить хотя бы так.
+}*/
+//Проблема с этим заданием не понял до конца как можно выполнить условие, по этому решил выполнить хотя бы так.
