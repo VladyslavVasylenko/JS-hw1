8b96fd1e94f11c0a9d10284af1168eacc0627101
VladyslavVasylenko <vladyslavvasylenko03@gmail.com>
2021-04-11 16:23:02 +0300

    hw-js-5

--- hw-js-5/hw-js-5-4/script.js
+++ hw-js-5/hw-js-5-4/script.js
@@ -1,2 +1,37 @@
 "use strict";
 
+let audAcadem = [
+  { item_tab: 'Название аудитории', item_aud: 231, num_seats: 10, item_facult:"Web"},
+  { item_tab: 'Название аудитории', item_aud: 251, num_seats: 14, item_facult:"Designer"},
+  { item_tab: 'Название аудитории', item_aud: 271, num_seats: 20, item_facult:"Developer"},
+  { item_tab: 'Название аудитории', item_aud: 131, num_seats: 10, item_facult:"Web"},
+  { item_tab: 'Название аудитории', item_aud: 151, num_seats: 12, item_facult:"Designer"},
+  { item_tab: 'Название аудитории', item_aud: 171, num_seats: 18, item_facult:"Developer"},
+];
+
+function audience (){
+  for(let info of audAcadem) {
+    console.log (`${info.item_tab}, ${info.item_aud}`);
+  }
+  console.log("\n");
+};
+
+audience();
+
+function seat (){
+  for(let info of audAcadem) {
+    console.log (`${info.item_tab}, ${info.item_aud}, количесто мест: ${info.num_seats}`);
+  }
+  console.log("\n");
+};
+
+seat ();
+
+function faculty (){
+  for(let info of audAcadem) {
+    console.log (`${info.item_tab}, ${info.item_aud}, факультет: ${info.item_facult} `);
+  }
+  console.log("\n");
+};
+
+faculty ()
