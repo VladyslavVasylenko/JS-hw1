9e3f7093ea3f5020b1ad66e0ac71c0138238dae8
VldyslavVasylenko <vladyslavvasylenko03@gmail.com>
2021-04-12 22:13:10 +0300

    hw-js-5-3

--- hw-js-5/hw-js-5-3/script.js
+++ hw-js-5/hw-js-5-3/script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const style = [
+  { id: 1, fontFamily: 'font-family: Franklin Gothic Medium, Arial Narrow, Arial, sans-serif;',},
+  { id: 2, fontSize: 'font-size:18px;',},
+  { id: 3, color:'color:green;',},
+  { id: 4, textAlign:'text-align:center;',},
+  { id: 5, fontStyle: 'font-style:italic;',},
+  { id: 6, textDecoration: 'text-decoration: underline;',},
+]
+
+function documentStyle (){
+  let fontFam = style.find(item => item.id == 1);
+  let fontSiz = style.find(item => item.id == 2);
+  let color = style.find(item => item.id == 3);
+  let textAlign = style.find(item => item.id == 4);
+  let fontSty = style.find(item => item.id == 5);
+  let textDecoration = style.find(item => item.id == 6);
+
+  document.write('<p style="' + textDecoration.textDecoration + ' ' + fontFam.fontFamily + ' ' + fontSty.fontStyle + ' ' + color.color + ' ' + textAlign.textAlign + ' ' + fontSiz.fontSize + ' ' + '">' + 'Hello World is I Vlad! I want to know JS!' + '</p>');
+}
+
+documentStyle();
