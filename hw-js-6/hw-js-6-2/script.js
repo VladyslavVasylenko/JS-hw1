29303b94a40128917d06f5104431af33a544c5c4
VladyslavVasylenko <vladyslavvasylenko03@gmail.com>
2021-04-15 16:26:10 +0300

    fixes

--- hw-js-6/hw-js-6-2/script.js
+++ hw-js-6/hw-js-6-2/script.js
@@ -4,27 +4,18 @@ function open(){
   alert('Very interesting homework for me.');
 }
 
-document.getElementsByClassName('buttonOne')[0].addEventListener('click', mModal);
-function mModal (){
-  alert('Very interesting homework for me.');
-}
-
+//next window
 let modal = document.getElementById('myModal');
-
 let btn = document.getElementById("myBtn");
-
 let closed = document.getElementsByClassName("close")[0];
-
 
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-
 closed.onclick = function() {
   modal.style.display = "none";
 }
-
 
 window.onclick = function(event) {
   if (event.target == modal) {
