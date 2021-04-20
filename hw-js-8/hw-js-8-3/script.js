b2bb571b95a32e78faf7b36755cb622a402a3ca1
unknown <vladyslavvasylenko03@gmail.com>
2021-04-20 14:44:20 +0300

    hw-js-8-3

--- hw-js-8/hw-js-8-3/script.js
+++ hw-js-8/hw-js-8-3/script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const pullContent = document.getElementById('content-textarea');
+
+const resizer = document.querySelector('div.content__textarea--resizer');
+
+const initResize = e => {
+  e.preventDefault;
+  window.addEventListener('mousemove', startResize);
+  window.addEventListener('mouseup', stopResize);
+};
+
+const startResize = e => {
+  pullContent.style.width = (e.clientX - pullContent.offsetLeft) + "px";
+};
+
+const stopResize = e => {
+  window.removeEventListener('mousemove', startResize);
+  window.removeEventListener('mouseup', stopResize);
+};
+
+resizer.addEventListener('mousedown', initResize);
