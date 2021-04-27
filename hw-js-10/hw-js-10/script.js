507108a499754c0efd5a3d393e4b7afdfeb84942
unknown <vladyslavvasylenko03@gmail.com>
2021-04-27 15:57:36 +0300

    hw-js-10-slick

---
+++ hw-js-10/hw-js-10/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+$('.works-slider').slick({
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	centerMode: true,
+	prevArrow:'<div class="prev-arrow" src="./image/prew.svg"><i class="fas fa-chevron-circle-left"></i></div>',
+	nextArrow:'<div class="next-arrow" src="./image/next.svg"><i class="fas fa-chevron-circle-right"></i></div>',
+});
--- hw-js-2/hw-js-2-8/script.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-let currDay = 0;
-
-while (confirm(`${days[currDay]}. Хотите увидеть следующий день?`)) {
-  currDay = (currDay + 1) % days.length;
-}
