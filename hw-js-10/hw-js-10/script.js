e5f7a5af03d328aeb63171aeaab190aa8ee891f2
unknown <vladyslavvasylenko03@gmail.com>
2021-04-28 11:13:44 +0300

    fixes

--- hw-js-10/hw-js-10/script.js
+++ hw-js-10/hw-js-10/script.js
@@ -5,6 +5,6 @@ $('.works-slider').slick({
 	slidesToShow: 3,
 	slidesToScroll: 1,
 	centerMode: true,
-	prevArrow:'<div class="prev-arrow" src="./image/prew.svg"><i class="fas fa-chevron-circle-left"></i></div>',
-	nextArrow:'<div class="next-arrow" src="./image/next.svg"><i class="fas fa-chevron-circle-right"></i></div>',
+	prevArrow:'<div class="prev-arrow"><i class="fas fa-chevron-circle-left"></i></div>',
+	nextArrow:'<div class="next-arrow"><i class="fas fa-chevron-circle-right"></i></div>',
 });
